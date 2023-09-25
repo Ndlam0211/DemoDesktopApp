078ce057236f9e8226c17954bb54e112432f864e
Nguyễn Đình Lâm <117760972+Ndlam0211@users.noreply.github.com>
2023-09-25 17:05:46 +0700

    Update Form4.cs

--- NguyenDinhLam_2122110509/Form4.cs
+++ NguyenDinhLam_2122110509/Form4.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +16,63 @@ namespace NguyenDinhLam_2122110509
         {
             InitializeComponent();
         }
+
+        float data1,data2;
+        string calculation;
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            view.Text = view.Text + "0";
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            view.Text = view.Text + "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            view.Text = view.Text + "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            view.Text = view.Text + "3";
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            calculation = "plus"; 
+            data1 = float.Parse(view.Text);
+            view.Clear();
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e) 
+        {
+            if (calculation == "plus")
+            {
+                data2 = data1 + float.Parse(view.Text);
+                view.Text = data2.ToString();
+            }
+
+            if (calculation == "multi")
+            {
+                data2 = data1 * float.Parse(view.Text);
+                view.Text = data2.ToString();
+            }
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            calculation = "multi";
+            data1 = float.Parse(view.Text);
+            view.Clear(); 
+        }
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            view.Text = view.Text + ".";
+        }
     }
 }
+ 
