4743d794d5a446fa153f0c4f569838c4c261499e
Nguyen Dinh Lam <nguyendinhlam0211@gmail.com>
2023-10-05 16:17:24 +0700

    update Form7

--- NguyenDinhLam_2122110509/Form7.Designer.cs
+++ NguyenDinhLam_2122110509/Form7.Designer.cs
@@ -51,9 +51,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(10, 13);
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 19);
+            this.label1.Size = new System.Drawing.Size(108, 23);
             this.label1.TabIndex = 0;
             this.label1.Text = "Student ID:";
             // 
@@ -61,9 +62,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(10, 47);
+            this.label2.Location = new System.Drawing.Point(13, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 19);
+            this.label2.Size = new System.Drawing.Size(66, 23);
             this.label2.TabIndex = 1;
             this.label2.Text = "Name:";
             // 
@@ -71,9 +73,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(10, 81);
+            this.label3.Location = new System.Drawing.Point(13, 100);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 19);
+            this.label3.Size = new System.Drawing.Size(71, 23);
             this.label3.TabIndex = 2;
             this.label3.Text = "Grade:";
             // 
@@ -81,39 +84,43 @@
             // 
             this.txtID.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtID.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtID.Location = new System.Drawing.Point(103, 13);
+            this.txtID.Location = new System.Drawing.Point(137, 16);
+            this.txtID.Margin = new System.Windows.Forms.Padding(4);
             this.txtID.Multiline = true;
             this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(151, 20);
+            this.txtID.Size = new System.Drawing.Size(201, 25);
             this.txtID.TabIndex = 3;
             // 
             // txtGrade
             // 
             this.txtGrade.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtGrade.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtGrade.Location = new System.Drawing.Point(103, 79);
+            this.txtGrade.Location = new System.Drawing.Point(137, 97);
+            this.txtGrade.Margin = new System.Windows.Forms.Padding(4);
             this.txtGrade.Multiline = true;
             this.txtGrade.Name = "txtGrade";
-            this.txtGrade.Size = new System.Drawing.Size(95, 20);
+            this.txtGrade.Size = new System.Drawing.Size(127, 25);
             this.txtGrade.TabIndex = 4;
             // 
             // txtName
             // 
             this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtName.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.Location = new System.Drawing.Point(103, 45);
+            this.txtName.Location = new System.Drawing.Point(137, 55);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4);
             this.txtName.Multiline = true;
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(372, 20);
+            this.txtName.Size = new System.Drawing.Size(496, 25);
             this.txtName.TabIndex = 5;
             // 
             // btnAdd
             // 
             this.btnAdd.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnAdd.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.Location = new System.Drawing.Point(14, 131);
+            this.btnAdd.Location = new System.Drawing.Point(19, 161);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(52, 34);
+            this.btnAdd.Size = new System.Drawing.Size(69, 42);
             this.btnAdd.TabIndex = 6;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -123,42 +130,49 @@
             // 
             this.btnEdit.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnEdit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEdit.Location = new System.Drawing.Point(81, 131);
+            this.btnEdit.Location = new System.Drawing.Point(108, 161);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(4);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(52, 34);
+            this.btnEdit.Size = new System.Drawing.Size(69, 42);
             this.btnEdit.TabIndex = 7;
             this.btnEdit.Text = "Edit";
             this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
             // 
             // btnDel
             // 
             this.btnDel.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnDel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDel.Location = new System.Drawing.Point(148, 131);
+            this.btnDel.Location = new System.Drawing.Point(197, 161);
+            this.btnDel.Margin = new System.Windows.Forms.Padding(4);
             this.btnDel.Name = "btnDel";
-            this.btnDel.Size = new System.Drawing.Size(66, 34);
+            this.btnDel.Size = new System.Drawing.Size(88, 42);
             this.btnDel.TabIndex = 8;
             this.btnDel.Text = "Delete";
             this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
             // 
             // btnSort
             // 
             this.btnSort.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnSort.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSort.Location = new System.Drawing.Point(230, 131);
+            this.btnSort.Location = new System.Drawing.Point(307, 161);
+            this.btnSort.Margin = new System.Windows.Forms.Padding(4);
             this.btnSort.Name = "btnSort";
-            this.btnSort.Size = new System.Drawing.Size(52, 34);
+            this.btnSort.Size = new System.Drawing.Size(69, 42);
             this.btnSort.TabIndex = 9;
             this.btnSort.Text = "Sort";
             this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
             // 
             // btnSearch
             // 
             this.btnSearch.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnSearch.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSearch.Location = new System.Drawing.Point(303, 193);
+            this.btnSearch.Location = new System.Drawing.Point(404, 238);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(91, 34);
+            this.btnSearch.Size = new System.Drawing.Size(121, 42);
             this.btnSearch.TabIndex = 10;
             this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = true;
@@ -167,11 +181,13 @@
             // 
             this.txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtSearch.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSearch.Location = new System.Drawing.Point(14, 193);
+            this.txtSearch.Location = new System.Drawing.Point(19, 238);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4);
             this.txtSearch.Multiline = true;
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(268, 34);
+            this.txtSearch.Size = new System.Drawing.Size(357, 42);
             this.txtSearch.TabIndex = 11;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
             // 
             // dataGridStudent
             // 
@@ -180,15 +196,19 @@
             this.ColID,
             this.ColName,
             this.ColGrade});
-            this.dataGridStudent.Location = new System.Drawing.Point(12, 244);
+            this.dataGridStudent.Location = new System.Drawing.Point(16, 300);
+            this.dataGridStudent.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridStudent.Name = "dataGridStudent";
-            this.dataGridStudent.Size = new System.Drawing.Size(467, 194);
+            this.dataGridStudent.RowHeadersWidth = 51;
+            this.dataGridStudent.Size = new System.Drawing.Size(623, 239);
             this.dataGridStudent.TabIndex = 12;
+            this.dataGridStudent.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridStudent_CellClick);
             // 
             // ColID
             // 
             this.ColID.DataPropertyName = "ID";
             this.ColID.HeaderText = "Student ID";
+            this.ColID.MinimumWidth = 6;
             this.ColID.Name = "ColID";
             this.ColID.Width = 125;
             // 
@@ -196,6 +216,7 @@
             // 
             this.ColName.DataPropertyName = "Name";
             this.ColName.HeaderText = "Name";
+            this.ColName.MinimumWidth = 6;
             this.ColName.Name = "ColName";
             this.ColName.Width = 200;
             // 
@@ -203,13 +224,15 @@
             // 
             this.ColGrade.DataPropertyName = "Grade";
             this.ColGrade.HeaderText = "Grade";
+            this.ColGrade.MinimumWidth = 6;
             this.ColGrade.Name = "ColGrade";
+            this.ColGrade.Width = 125;
             // 
             // Form7
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(491, 450);
+            this.ClientSize = new System.Drawing.Size(655, 554);
             this.Controls.Add(this.dataGridStudent);
             this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.btnSearch);
@@ -223,6 +246,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form7";
             this.Text = "Student Management";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridStudent)).EndInit();
--- NguyenDinhLam_2122110509/Form7.cs
+++ NguyenDinhLam_2122110509/Form7.cs
@@ -54,6 +54,78 @@ namespace NguyenDinhLam_2122110509
                 grade = float.Parse(txtGrade.Text);
                 Student student = new Student(id, name, grade);
                 students.Add(student);
+                dataGridStudent.DataSource = null;
+                dataGridStudent.DataSource = students;
+                txtID.Text = null;
+                txtName.Text = null;
+                txtGrade.Text = null;
+
+            }
+        }
+
+        int index;
+
+        private void dataGridStudent_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            index = e.RowIndex;
+            if (index>=0)
+            {
+                txtID.Text = students[index].ID.ToString();
+                txtName.Text = students[index].Name;
+                txtGrade.Text = students[index].Grade.ToString();
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (index>=0)
+            {
+                students[index].ID = Int32.Parse(txtID.Text);
+                students[index].Name = txtName.Text;
+                students[index].Grade = float.Parse(txtGrade.Text);
+                dataGridStudent.DataSource = null;
+                dataGridStudent.DataSource = students;
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("do you certainly remove?", "warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+            {
+                students.RemoveAt(index);
+                dataGridStudent.DataSource = null;
+                dataGridStudent.DataSource = students;
+            }
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            students.Sort((a, b) => a.Grade.CompareTo(b.Grade));
+            dataGridStudent.DataSource = null;
+            dataGridStudent.DataSource = students;
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            string value = txtSearch.Text;
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                dataGridStudent.DataSource = null;
+                dataGridStudent.DataSource = students;
+                List<Student> tmp = new List<Student>();
+                for (int i = 0; i < dataGridStudent.Rows.Count; i++)
+                {
+                    if (dataGridStudent.Rows[i].Cells[1].Value.ToString().ToLower().Contains(value.ToLower()))
+                    {
+                        tmp.Add(students[i]);
+                    }
+                }
+                dataGridStudent.DataSource = null;
+                dataGridStudent.DataSource = tmp;
+            }
+            else
+            {
+                dataGridStudent.DataSource = null;
                 dataGridStudent.DataSource = students;
             }
         }
